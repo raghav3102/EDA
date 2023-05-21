f3add1686389b5a2191d8380bd4bba90a9eb1550
raghav3102 <107983742+raghav3102@users.noreply.github.com>
2023-05-21 16:23:56 +0530

    Update and rename crime.Rmd to crime.r

---
+++ crime.r
@@ -0,0 +1,37 @@
+data <- read.csv("D:/Sem 6/EDA/Crimes_-_2001_to_Present_refined.csv")
+
+install.packages("ggplot2")
+
+library(ggplot2)
+
+ggplot(data,aes(x=Beat))+geom_histogram()
+
+ggplot(data,aes(x=Beat))+geom_histogram(fill="white",color="black")
+
+ggplot(data,aes(x=Beat))+geom_density()
+
+ggplot(data,aes(x=Beat))+geom_boxplot()
+
+ggplot(data,aes(y=Beat))+geom_bar()
+
+ggplot(data,aes(x=ID,y=Beat))+geom_step()
+
+ggplot(data,aes(x=ID,y=Beat))+geom_count()
+
+ggplot(data,aes(x=ID,y=Beat))+geom_area()
+
+ggplot(data,aes(x=ID,y=Beat))+geom_line()
+
+ggplot(data,aes(x=ID))+geom_blank()
+
+idv = data[["ID"]]
+bv = data[["Beat"]]
+plot(x=idv,y=bv)
+
+ggplot(data,aes(x=ID,y=Beat))+geom_point()
+
+ggplot(data, aes(x = ID, y = Beat)) +
+        geom_point() +
+        stat_smooth(method = "lm",
+        col = "#C42126", se = FALSE, size = 1
+)
